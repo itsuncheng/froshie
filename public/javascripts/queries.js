0148f2ab4368a73e254239f3b64839afa383068d
Raymond <itsuncheng2000@gmail.com>
2018-08-17 18:16:09 +0800

    heroku post

--- public/javascripts/queries.js
+++ public/javascripts/queries.js
@@ -7,52 +7,63 @@ var options = {
   promiseLib: promise
 };
 
+// var pgp = require('pg-promise')(options);
+// const cn = {
+//     host: 'localhost', // 'localhost' is the default;
+//     port: 5432, // 5432 is the default;
+//     database: 'mytestdb',
+//     user: 'raymond',
+//     password: 'raymond923'
+// };
+// var db = pgp(cn);
+
+
 var pgp = require('pg-promise')(options);
 const cn = {
-    host: 'localhost', // 'localhost' is the default;
+    host: 'ec2-107-21-98-165.compute-1.amazonaws.com', // 'localhost' is the default;
     port: 5432, // 5432 is the default;
-    database: 'mytestdb',
-    user: 'raymond',
-    password: 'raymond923'
+    database: 'deuqt6ucu403e0',
+    user: 'sbvblqzpwodrow',
+    password: 'c65601ca99c5237c9ab0e86f7a842ca4a542fcd1682123cd0d1a4c1052530127'
 };
 var db = pgp(cn);
 
 
 
-//Heroku Postgres
-const { Client } = require('pg');
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-client.connect();
+// //Heroku Postgres
+// const { Client } = require('pg');
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: true,
+// });
+// client.connect();
+//
+// function getAllRecords(req, res, next) {
+//   var result = {};
+//   client.query('SELECT * from IB;', (err, result) => {
+//     if (err) throw err;
+//     res.status(200).send(result.rows);
+//     client.end();
+//   });
+// }
+
+
 
 function getAllRecords(req, res, next) {
-  var result = {};
-  client.query('SELECT * from IB;', (err, result) => {
-    if (err) throw err;
-    res.status(200).send(result.rows);
-    client.end();
-  });
+  db.any('select * from IB')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Retrieved ALL records'
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+  //res.send("Hello")
 }
-
-
-
-// function getAllRecords(req, res, next) {
-//   db.any('select * from IB')
-//     .then(function (data) {
-//       res.status(200)
-//         .json({
-//           status: 'success',
-//           data: data,
-//           message: 'Retrieved ALL records'
-//         });
-//     })
-//     .catch(function (err) {
-//       return next(err);
-//     });
-//   //res.send("Hello")
-// }
 
 function insertRecord(req, res, next) {
 
